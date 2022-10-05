3e22ec3cc76cfd71c5aca45a186488b869534281
Bradley Wells <support@wellsb.com>
2022-10-05 14:10:06 +0000

    Add reconnect command

--- WellsChat.ClientConsole/Program.cs
+++ WellsChat.ClientConsole/Program.cs
@@ -13,6 +13,8 @@ namespace WellsChat.Clientconsole
     {
         static string _accessToken = string.Empty;
         static SecretClient secretClient = null;
+        static HubConnection hubConnection = null;
+        static Aes256Cipher cipher = null;
         static User me = null;
         static async Task Main(string[] args) {
             Console.WriteLine("Authenticating...");
@@ -23,11 +25,58 @@ namespace WellsChat.Clientconsole
 
             var credentials = new DefaultAzureCredential(true);
             secretClient = new SecretClient(new Uri(config.GetValue<string>("VaultUri")), credentials);
-            
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            Console.WriteLine("Authenticated    ");
 
-            var cipher = new Aes256Cipher(Convert.FromBase64String(secretClient.GetSecret("Key").Value.Value));            
+            cipher = new Aes256Cipher(Convert.FromBase64String(secretClient.GetSecret("Key").Value.Value));
 
-            IPublicClientApplication app = PublicClientApplicationBuilder.Create(secretClient.GetSecret("ClientId").Value.Value) 
+            if (await EstablishConnection()) {
+                while (true)
+                {
+                    Console.Write("> ");
+                    Message message = new() { Payload = Console.ReadLine() };
+
+                    if (message.Payload.ToLower() == "exit")
+                    {
+                        await hubConnection.DisposeAsync();
+                        Environment.Exit(0);
+                    }
+                    else if(message.Payload.ToLower() == "!reconnect")
+                    {
+                        await EstablishConnection();
+                    }
+                    else
+                    {
+                        if (!string.IsNullOrWhiteSpace(message.Payload))
+                        {
+                            if (message.Payload.ToLower() != "!users") //do not encrypt command messages
+                            {
+                                message.SenderEmail = me.Email;
+                                message.SenderDisplayName = me.DisplayName;
+                                message = cipher.EncryptMessage(message);
+                            }
+
+                            try
+                            {
+                                await hubConnection.SendAsync("SendMessage", message);
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Message not sent");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private static async Task<bool> EstablishConnection()
+        {
+            if (hubConnection != null && hubConnection.State != HubConnectionState.Disconnected)
+                await hubConnection.StopAsync();
+                
+
+            Console.WriteLine("Connecting...");
+            IPublicClientApplication app = PublicClientApplicationBuilder.Create(secretClient.GetSecret("ClientId").Value.Value)
                 .WithDefaultRedirectUri()
                 .WithAuthority(AadAuthorityAudience.AzureAdMyOrg)
                 .WithTenantId(secretClient.GetSecret("TenantId").Value.Value)
@@ -71,122 +120,101 @@ namespace WellsChat.Clientconsole
                     Environment.Exit(0);
                 }
             }
-            
+
 
             if (result != null)
             {
                 _accessToken = result.AccessToken;
-                me = new User() { 
+                me = new User()
+                {
                     Email = result.ClaimsPrincipal.FindFirst("preferred_username").Value, //email
-                    DisplayName = result.ClaimsPrincipal.FindFirst("name").Value 
-                };                
+                    DisplayName = result.ClaimsPrincipal.FindFirst("name").Value
+                };
 
-                var hubConnection = new HubConnectionBuilder()
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+                Console.WriteLine("Connected    ");
+                hubConnection = new HubConnectionBuilder()
                     .WithUrl(secretClient.GetSecret("HubUrl").Value.Value, options =>
                     {
                         options.AccessTokenProvider = () => Task.FromResult(_accessToken);
                     })
                     .WithAutomaticReconnect()
                     .Build();
-                Console.SetCursorPosition(0, Console.CursorTop - 1);
-                Console.WriteLine("Authenticated    ");
+
                 try
                 {
-                    Console.WriteLine("Connecting...");
+                    AddHandlers();
                     await hubConnection.StartAsync();
                     Console.SetCursorPosition(0, Console.CursorTop - 1);
                     Console.WriteLine("Connected    ");
+                    return true;
                 }
                 catch (HttpRequestException e)
                 {
                     Console.WriteLine("Error connecting to server.");
                     Console.ReadLine();
                     Environment.Exit(0);
-                }
-
-            
-                hubConnection.On<User>("UserConnected", (user) =>
-                {
-                    Console.SetCursorPosition(0, Console.CursorTop);
-                    Console.BackgroundColor = ConsoleColor.DarkCyan;
-                    Console.Write($"{DateTime.Now.ToString("g")} | {user.Email} ({user.DisplayName}) [{user.ActiveConnections}] connected.");
-                    Console.ResetColor();
-                    Console.WriteLine();
-                    Console.Write("> ");
-                });
-
-                hubConnection.On<User>("UserDisconnected", (user) =>
-                {
-                    Console.SetCursorPosition(0, Console.CursorTop);
-                    Console.BackgroundColor = ConsoleColor.DarkGray;
-                    Console.Write($"{DateTime.Now.ToString("g")} | {user.Email} ({user.DisplayName}) [{user.ActiveConnections}] disconnected.");
-                    Console.ResetColor();
-                    Console.WriteLine();
-                    Console.Write("> ");
-                });
-
-                hubConnection.On<Message>("ReceiveMessage", (message) =>
-                {                    
-                    message = cipher.DecryptMessage(message);
-                    bool isMe = message.SenderEmail == me.Email;
-                    var receivedMsg = $"{DateTime.Now.ToString("g")} | {message.SenderDisplayName}: {message.Payload}";
-                    Console.SetCursorPosition(0, Console.CursorTop);
-                    Console.BackgroundColor = isMe ? ConsoleColor.DarkGreen : ConsoleColor.DarkBlue;
-                    Console.Write(receivedMsg);
-                    Console.ResetColor();
-                    Console.WriteLine(message.SenderEmail == me.Email ? null : "\a");
-                    Console.Write("> ");
-                });
-
-                hubConnection.On("SendSuccess", () =>
-                {                   
-                    Console.SetCursorPosition(0, Console.CursorTop - 1);
-                });
-
-                hubConnection.On<List<User>>("ListUsers", (users) =>
-                {
-                    Console.SetCursorPosition(0, Console.CursorTop);
-                    Console.WriteLine($"{users.Count} users online");
-                    foreach(var user in users)
-                    {
-                        Console.WriteLine($"{user.Email} ({user.DisplayName}) [{user.ActiveConnections}]");
-                    }
-                    Console.Write("> ");
-                });            
-
-                while (true)
-                {
-                    Console.Write("> ");
-                    Message message = new() { Payload = Console.ReadLine() };
-
-                    if (message.Payload.ToLower() == "exit")
-                    {
-                        await hubConnection.DisposeAsync();
-                        Environment.Exit(0);
-                    }
-                    else
-                    {
-                        if (!string.IsNullOrWhiteSpace(message.Payload))
-                        {
-                            if (message.Payload.ToLower() != "!users") //do not encrypt command messages
-                            {                                
-                                message.SenderEmail = me.Email;
-                                message.SenderDisplayName = me.DisplayName;
-                                message = cipher.EncryptMessage(message);
-                            }
-
-                            try
-                            {
-                                await hubConnection.SendAsync("SendMessage", message);
-                            }
-                            catch
-                            {
-                                Console.WriteLine("Message not sent");
-                            }
-                        }
-                    }
+                    return false;
                 }
             }
+            else
+            {
+                Console.WriteLine("Error connecting to server.");
+                Console.ReadLine();
+                Environment.Exit(0);
+                return false;
+            }
+        }
+        private static void AddHandlers()
+        {
+            hubConnection.On<User>("UserConnected", (user) =>
+            {
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.BackgroundColor = ConsoleColor.DarkCyan;
+                Console.Write($"{DateTime.Now.ToString("g")} | {user.Email} ({user.DisplayName}) [{user.ActiveConnections}] connected.");
+                Console.ResetColor();
+                Console.WriteLine();
+                Console.Write("> ");
+            });
+
+            hubConnection.On<User>("UserDisconnected", (user) =>
+            {
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.BackgroundColor = ConsoleColor.DarkGray;
+                Console.Write($"{DateTime.Now.ToString("g")} | {user.Email} ({user.DisplayName}) [{user.ActiveConnections}] disconnected.");
+                Console.ResetColor();
+                Console.WriteLine();
+                Console.Write("> ");
+            });
+
+            hubConnection.On<Message>("ReceiveMessage", (message) =>
+            {
+                message = cipher.DecryptMessage(message);
+                bool isMe = message.SenderEmail == me.Email;
+                var receivedMsg = $"{DateTime.Now.ToString("g")} | {message.SenderDisplayName}: {message.Payload}";
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.BackgroundColor = isMe ? ConsoleColor.DarkGreen : ConsoleColor.DarkBlue;
+                Console.Write(receivedMsg);
+                Console.ResetColor();
+                Console.WriteLine(message.SenderEmail == me.Email ? null : "\a");
+                Console.Write("> ");
+            });
+
+            hubConnection.On("SendSuccess", () =>
+            {
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+            });
+
+            hubConnection.On<List<User>>("ListUsers", (users) =>
+            {
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.WriteLine($"{users.Count} users online");
+                foreach (var user in users)
+                {
+                    Console.WriteLine($"{user.Email} ({user.DisplayName}) [{user.ActiveConnections}]");
+                }
+                Console.Write("> ");
+            });
         }
     }
 }
