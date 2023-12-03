786d36362c13ca273b00a4532a966c72750e77e1
bradwellsb <support@wellsb.com>
2023-12-03 15:42:25 +0100

    Upgrade to .NET 8

--- CohoistChat.Functions/KeyVault.cs
+++ CohoistChat.Functions/KeyVault.cs
@@ -2,20 +2,26 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Azure.Identity;
 using System.Text.Json;
 using Azure.Security.KeyVault.Secrets;
+using Microsoft.Azure.Functions.Worker;
 
 namespace CohoistChat.Functions
 {
-    public static class KeyVault
+    public class KeyVault
     {
-        [FunctionName("KeyVault")]
-        public static async Task<IActionResult> Run(
+        private readonly ILogger _logger;
+
+        public KeyVault(ILogger<KeyVault> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("KeyVault")]
+        public async Task<IActionResult> Run(
                        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req)
         {
             var key = req.Query["key"];
